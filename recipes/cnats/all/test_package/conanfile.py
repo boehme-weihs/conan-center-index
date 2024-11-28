9b645eac124bc4967557fc114cb409f865febd38
Laurin Harke <84014975+har-bw@users.noreply.github.com>
2024-11-28 09:53:13 +0100

    Update conanfile.py

--- recipes/cnats/all/test_package/conanfile.py
+++ recipes/cnats/all/test_package/conanfile.py
@@ -15,15 +15,8 @@ class TestPackageConan(ConanFile):
     def layout(self):
         cmake_layout(self)
 
-    @property
-    def _nats_library_name(self):
-        suffix = "" if self.dependencies["cnats"].options.shared else "_static"
-        debug = "d" if self.dependencies["cnats"].settings.build_type == "Debug" else ""
-        return f"nats{suffix}{debug}"
-
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["CNATS_TARGET"] = self._nats_library_name
         tc.generate()
 
     def build(self):
